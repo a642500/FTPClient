0065b906352734fbd7d1e38ebe3583a864c9148d
a642500 <toxz@foxmail.com>
2015-10-22 10:39:11 +0800

    FTPClient basic test

---
+++ test/me/toxz/ftp/client/FTPClientTest.java
@@ -0,0 +1,26 @@
+package me.toxz.ftp.client;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.IOException;
+
+/**
+ * Created by yyz on 10/21/15.
+ */
+public class FTPClientTest {
+    private FTPClient client;
+
+    @Before
+    public void createClient() {
+        client = new FTPClient();
+    }
+
+
+    @Test
+    public void testConnect() throws IOException {
+        client.connect("127.0.0.1", 21);
+    }
+
+}
