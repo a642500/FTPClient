9e6d694cc396f3f4de2f42bf0ca88dd60dd03543
a642500 <toxz@foxmail.com>
2015-10-22 10:39:41 +0800

    add partly fxml and controller

---
+++ src/sample/Controller.java
@@ -0,0 +1,17 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class Controller {
+
+    public Controller() {
+
+        
+    }
+
+    @FXML
+    void onAnonymousChecked(ActionEvent actionEvent) {
+
+    }
+}
