0d5321c8f5d9f506671657cce5b17f680ea646bd
Carlos <toxz@foxmail.com>
2015-10-24 17:53:33 +0800

    implement

--- src/me/toxz/ftp/sample/Main.java
+++ src/me/toxz/ftp/sample/Main.java
@@ -5,16 +5,34 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import me.toxz.ftp.client.FTPClient;
 import me.toxz.ftp.model.User;
 
+import java.io.IOException;
+
 public class Main extends Application {
+    private User mUser;
+    private Stage mStage;
+    private FTPClient mClient;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 600, 400));
+        mClient = new FTPClient();
+
+
+        mStage = primaryStage;
+        mStage.setMinWidth(380);
+        mStage.setMinHeight(500);
+        gotoLogin();
+
+
         primaryStage.show();
+    }
+
+    private void gotoLogin() throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        mStage.setTitle("Login");
+        mStage.setScene(new Scene(root, 600, 400));
     }
 
 
@@ -23,7 +41,14 @@ public class Main extends Application {
     }
 
     boolean login(User user) {
-
-        return true;
+        try {
+            if (mUser.isAnonymous()) {
+                mClient.connect(user.getHost(), user.getPortValue(), user.getUserName(), user.getPassword());
+            }
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
