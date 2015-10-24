127183914d3734c525ca5736523f14ea9461c063
Carlos <toxz@foxmail.com>
2015-10-24 17:54:04 +0800

    update file explorer scene

---
+++ src/me/toxz/ftp/sample/FileExplorerController.java
@@ -0,0 +1,17 @@
+package me.toxz.ftp.sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+/**
+ * Created by Carlos on 2015/10/23.
+ */
+public class FileExplorerController {
+
+    @FXML TextField localPathText;
+    @FXML TextField remotePathText;
+    @FXML TextField currentIPText;
+    @FXML Button disconnectBtn;
+
+}
