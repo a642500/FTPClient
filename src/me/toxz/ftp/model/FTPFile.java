fcdaf10c1067db6d327a1f9428d56a4fcab39d3c
Carlos <toxz@foxmail.com>
2015-10-26 19:16:29 +0800

    add FTPFile parser

---
+++ src/me/toxz/ftp/model/FTPFile.java
@@ -0,0 +1,65 @@
+package me.toxz.ftp.model;
+
+import com.google.common.base.CharMatcher;
+import com.google.common.base.Splitter;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Spliterator;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+/**
+ * Created by Carlos on 2015/10/26.
+ */
+public class FTPFile {
+    private final String name;
+    private final String dir;
+    private final boolean isDir;
+    private final long size;
+    private final String permission;
+    private final String modifiedDate;
+
+    private FTPFile(String name, String dir, boolean isDir, long size, String permission, String modifiedDate) {
+        this.name = name;
+        this.dir = dir;
+        this.isDir = isDir;
+        this.size = size;
+        this.permission = permission;
+        this.modifiedDate = modifiedDate;
+    }
+
+    public static FTPFile format(String string, String dir) {
+        Spliterator<String> spliterator = Splitter.on(CharMatcher.BREAKING_WHITESPACE).split(string).spliterator();
+        Iterator<String> info = StreamSupport.stream(spliterator, false).filter((s) -> !s.isEmpty()).iterator();
+        String permission = info.next();
+        info.next();
+        info.next();
+        info.next();
+        long size = Long.parseLong(info.next());
+        String date = info.next() + " " + info.next() + " " + info.next();
+        String name = info.next();
+        boolean isDir = permission.contains("d");
+        FTPFile ftpFile = new FTPFile(name, dir, isDir, size, permission, date);
+        System.out.println(ftpFile);
+        return ftpFile;
+    }
+
+    @Override
+    public String toString() {
+        return "FTPFile{" +
+                "name='" + name + '\'' +
+                ", dir='" + dir + '\'' +
+                ", isDir=" + isDir +
+                ", size=" + size +
+                ", permission='" + permission + '\'' +
+                ", modifiedDate='" + modifiedDate + '\'' +
+                '}';
+    }
+
+    public static List<FTPFile> formatAll(String string, String dir) {
+        Spliterator<String> spliterator = Splitter.on(Pattern.compile("\\r?\\n")).split(string).spliterator();
+        return StreamSupport.stream(spliterator, false).filter(s1 -> !s1.isEmpty()).map(s -> format(s, dir)).collect(Collectors.toList());
+    }
+}
--- test/me/toxz/ftp/client/FTPClientTest.java
+++ test/me/toxz/ftp/client/FTPClientTest.java
@@ -1,9 +1,11 @@
 package me.toxz.ftp.client;
 
+import me.toxz.ftp.model.FTPFile;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.io.File;
 import java.io.IOException;
 
 /**
@@ -20,8 +22,19 @@ public class FTPClientTest {
 
     @Test
     public void testConnect() throws IOException {
+
+    }
+
+    @Test
+    public void testFormat() {
+        FTPFile.format("drwxr-xr-x    2 105      108          4096 Oct 26 09:49 upload", "/");
+    }
+
+    @Test
+    public void testFormatAll() throws IOException {
         client.connect("90.130.70.73", 21);
-        client.list();
+        String list = client.list();
+        System.out.println(FTPFile.formatAll(list, "/"));
     }
 
 }
